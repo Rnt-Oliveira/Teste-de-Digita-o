2eb33322ffda6d97e2f8a3e301c5020bf704e0e3
Renato Oliveira <renato.a.oliveira@outlook.com.br>
2025-09-13 01:53:58 -0300

    Add files via upload

---
+++ Projeto - Teste o quão rápido você digita/Velocidade de Digitação/script.js
@@ -0,0 +1,95 @@
+const testWrapper = document.querySelector(".test-wrapper");
+const testArea = document.querySelector("#test-area");
+const originText = document.querySelector("#origin-text p").innerHTML;
+const resetButton = document.querySelector("#reset");
+const theTimer = document.querySelector(".timer");
+
+timer = [0,0,0,0];
+var interval;
+var timerRunning = false;
+
+
+// Adiciona zero inicial aos números <= 9 (apenas para estética):
+function leadingZero(time) {
+    if (time <= 9){
+        time = "0" + time;
+
+    }
+    return time;
+}
+
+// Executa um timer padrão de minuto / segundo / centésimos:
+
+function runTimer() {
+    let currentTimer = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]); 
+    theTimer.innerHTML = currentTimer;
+    timer[3]++;
+
+    timer[0] = Math.floor((timer[3]/100)/60);
+    timer[1] = Math.floor((timer[3]/100) - (timer[0]*60));
+    timer[2] = Math.floor(timer[3] - (timer[1]*100) - (timer[0]*6000));
+
+}
+
+// Verifica se texto digitado com o fornecido na página:
+// Ainda falta fazer a mudança de cor funcionar corretamente.
+// Corrigido, foi só trocar o c minúsculo por c maiúsculo em borderColor. 
+function spellCheck() { 
+    let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0, textEntered.length);
+    if (textEntered == originText) {
+        clearInterval(interval);
+
+        testWrapper.style.borderColor = "#429890";
+   } else {
+        if (textEntered == originTextMatch) {
+            testWrapper.style.borderColor = "#65CCf3";
+        } else {
+            testWrapper.style.borderColor = "#E95D0F";
+        }
+    }
+    
+}
+
+// Inicia o cronômetro:
+function start(){
+    let textEnteredLength = testArea.value.length;
+    if (textEnteredLength === 0 && !timerRunning){
+        timerRunning = true;
+        interval = setInterval(runTimer, 10);
+    }  
+    console.log(textEnteredLength);
+}
+
+// Função de recomeçar:
+function reset() {
+    clearInterval(interval);
+    interval = null;
+    timer = [0,0,0,0];
+    timerRunning = false;
+
+    testArea.value = "";
+    theTimer.innerHTML = "00:00:00";
+    testWrapper.style.borderColor = "grey";
+
+//         ESSE CÓDIGO ABAIXO PODE SER USADO PARA ENCONTRAR ERRROS, BASTA SUBSTITUIR COM OS DADOS QUE PRECISA DESCOBRIR.
+
+
+   /* if(testWrapper.style.borderColor == "grey") {
+        console.info("Show de bola, a cor é: ", testWrapper.stye.borderColor);
+    } else{
+        console.error("problema nas cores.")
+    }
+    */
+    
+}
+
+// Listeners de eventos para entrada de teclado e o botão de recomeçar:
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
+
+
+
+
+
